5fe99bf86f45515b14cbc68abbe5116bf0486930
Vishwanatha Kamath Katapadi <namitha.289@gmail.com>
2022-05-06 23:04:52 -0400

    Added multiple descriptive quality comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,24 +1,31 @@
 // Assignment Code
+//Declaring Array variables for Special charaters, Uppercase letters, lowercase letters & numbers
 var specialCharactersArray=['\\','!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~',']'];
 var lowerCaseLettersArray=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCaseLettersArray=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbersArray=['0','1','2','3','4','5','6','7','8','9'];
-var possiblePassword;
+//declaring variable to hold the length of password entered by the user.
 var passwordLength;
+//declaring variable that holds random password based on user-selected criteria
+var possiblePassword;
 
 var generateBtn = document.querySelector("#generate");
 
+//function generatePassword() is defined that will prompt user to enter the length of password needed !
 function generatePassword(){
      passwordLength=0;
      var randomPassword="";
 
     passwordLength=window.prompt("How many charcters long would you like your password to be?\nPassword length must be 8 to 128 characters long!");
     console.log(passwordLength);
+    //If the user has entered any input then execute the code within If ! else exit if clicked Cancel !
     if(passwordLength)
     {
+        //check if the length entered by the user is valid else prompt the user to re enter the length!
         if(passwordLength >=8 && passwordLength <=128)
         {
             console.log("correct");
+            // invokes the passwordPrompts() function and assigns the return value to variabe ramdomPassword !
             randomPassword=passwordPrompts();
             console.log("password is:",randomPassword);
         }else{
@@ -26,6 +33,7 @@ function generatePassword(){
             window.alert("Invalid Password length !\nPassword length must be 8 to 128 characters long!");
             generatePassword();
         }
+        //return the ramdomPassword generated!!
         return randomPassword;
     }
     else{
@@ -33,7 +41,7 @@ function generatePassword(){
         return randomPassword;
     }
 }
-
+//function passwordPrompt() is defined that will ask the user to select few criteria to generate random password !
 function passwordPrompts(){
 
     var randomArray=[];
@@ -79,6 +87,7 @@ function passwordPrompts(){
     return possiblePassword;
 }
 // Write password to the #password input
+//function writePassword() is defined to invoke the generatePassword() function and update the generated password on to the screen !
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
